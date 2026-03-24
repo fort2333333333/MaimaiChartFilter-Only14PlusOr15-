b015360528d32567de0b538ed155314d5b97f743
Fort233 <forttrof333@gmail.com>
2026-03-24 11:39:40 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import random
 import streamlit as st
+from PIL import Image
 
 @st.cache_data
 def load_data():
@@ -113,13 +114,63 @@ def random_song(songs):
         st.text("璋卞笀: 涓嶆槸涔濋笩")
         st.text("鏃ユ湡: 鍝堝搱-涓栫晫-浣犲ソ (鍢诲樆)")
         st.text("瀹炲湪: 缂栦笉涓�  鍘讳簡: 鎵�浠� 鍒板簳鍐�: 浠� 涔堝ソ鍛�:閿� 鏂ゆ嫹: 锟斤拷�� 鎯冲悆: 閿熸枻鎷�")
+        st.button("再来一首")
 
-st.title("请您越级")
+def crop_cover(index,x,y, cropped_size,g):
+    cover = Image.open(full_cover_path(INFO[index]["cover"]))
+    cropped = cover.crop((x,y,x+cropped_size,y+cropped_size))
+    if g:
+        cropped = cropped.convert("L")
+    st.image(cropped,width=300)
+
+@st.dialog("曲绘猜歌")
+def random_cover(songs,CROP_SIZE,grey):
+    if st.button("再猜一首"):
+        if songs:
+            st.session_state.song = random.choice(songs)
+        else:
+            st.session_state.song = random.randint(0,87)
+        st.session_state.x = random.randint(0,190-CROP_SIZE)
+        st.session_state.y = random.randint(0,190-CROP_SIZE)
+    crop_cover(st.session_state.song,st.session_state.x,st.session_state.y,CROP_SIZE,grey)
+    if st.button("看看答案"):
+        print_cover(st.session_state.song)
+
+if "grey" not in st.session_state:
+    st.session_state.grey = False
+if "image_size" not in st.session_state:
+    st.session_state.image_size = 30
+if "after_rerun" not in st.session_state:
+    st.session_state.after_rerun = ""
+
+@st.dialog("设置")
+def setting():
+    setting_image_size = st.slider("曲绘猜歌:曲绘裁剪大小",min_value = 10,max_value = 50,value=st.session_state.image_size)
+    setting_grey = st.toggle("曲绘猜歌:显示黑白曲绘",value=st.session_state.grey)
+    if st.button("保存"):
+        st.session_state.image_size = setting_image_size
+        st.session_state.grey = setting_grey
+        st.session_state.after_rerun = "保存成功!"
+        st.rerun()
+
+if st.session_state.after_rerun:
+    st.toast(st.session_state.after_rerun)
+    st.session_state.after_rerun = ""
+
+col5, col6 = st.columns([9,1])
+with col5:
+    st.title("请您越级")
+with col6:
+    st.text("")
+    st.text("")
+    if st.button("设置"):
+        setting()
 songs = list(range(0, 88))
 col3, col4 = st.columns(2)
 with col3:
     filter_level = st.multiselect("筛选定数",["14.6","14.7","14.8","14.9","15.0"])
     filter_version = st.multiselect("筛选版本",VERSION)
+    filter_name = st.text_input("搜索曲名")
 filter_level_list = []
 filter_version_list = []
 for level in filter_level:
@@ -129,10 +180,11 @@ for version in filter_version:
 songs = [
     s for s in songs
     if (not filter_level or s in filter_level_list)
-    and (not filter_version or s in filter_version_list)]
+    and (not filter_version or s in filter_version_list)
+    and (not filter_name or filter_name.lower() in INFO[s]["title"].lower())]
 
 with col4:
-    condition_code = st.text_area("筛选其他", height=153, help="语法为 类别 运算符 数值 例如bpm>180, released_date<2026-01-01, break==100")
+    condition_code = st.text_area("筛选其他", height=236, help="语法为 类别 运算符 数值 例如bpm>180, released_date<2026-01-01, break==100")
 VALID_KEY = ["bpm","released_date","break","tap","hold","slide","touch","total","version","internal_level"]
 if condition_code:
     try:
@@ -190,8 +242,19 @@ TRANSLATE = {"定数":"internal_level","bpm":"bpm","版本":"version","日期":"
              "slide":"slide","touch":"touch","break":"break","物量":"total","正序":False,"倒序":True}
 songs = sort_by(TRANSLATE[sort_method],TRANSLATE[sort_reverse],songs)
 
-if st.button("随机抽取"):
-    random_song(songs)
+col10, col11, col12, col13 = st.columns(4)
+with col10:
+    if st.button("随机抽取"):
+        random_song(songs)
+with col12:
+    if st.button("曲绘猜歌"):
+        if songs:
+            st.session_state.song = random.choice(songs)
+        else:
+            st.session_state.song = random.randint(0,87)
+        st.session_state.x = random.randint(0, 190 - st.session_state.image_size)
+        st.session_state.y = random.randint(0, 190 - st.session_state.image_size)
+        random_cover(songs,st.session_state.image_size,st.session_state.grey)
 try:
     print_all(songs)
 except Exception as e:
